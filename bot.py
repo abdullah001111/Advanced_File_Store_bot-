c3d681e279b5f55d3fc55da3468d01248d815140
Nozel Silva <97865856+Sahil0976@users.noreply.github.com>
2023-11-05 13:22:04 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ from pyrogram.enums import ParseMode
 import sys
 from datetime import datetime
 
-from config import API_HASH, APP_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS, FORCESUB_CHANNEL, FORCESUB_CHANNEL2, FORCESUB_CHANNEL3, CHANNEL_ID, PORT
+from config import API_HASH, APP_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS, FORCESUB_CHANNEL, FORCESUB_CHANNEL2, CHANNEL_ID, PORT
 
 class Bot(Client):
     def __init__(self):
@@ -56,19 +56,6 @@ class Bot(Client):
                 self.LOGGER(__name__).warning(f"Please Double check the FORCESUB_CHANNEL2 value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCESUB_CHANNEL2}")
                 self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/Hunters_Discussion for support")
                 sys.exit()
-        if FORCESUB_CHANNEL3:
-            try:
-                link = (await self.get_chat(FORCESUB_CHANNEL3)).invite_link
-                if not link:
-                    await self.export_chat_invite_link(FORCESUB_CHANNEL3)
-                    link = (await self.get_chat(FORCESUB_CHANNEL3)).invite_link
-                self.invitelink3 = link
-            except Exception as a:
-                self.LOGGER(__name__).warning(a)
-                self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
-                self.LOGGER(__name__).warning(f"Please Double check the FORCESUB_CHANNEL3 value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCESUB_CHANNEL3}")
-                self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/Hunters_Discussion for support")
-                sys.exit()       
         try:
             db_channel = await self.get_chat(CHANNEL_ID)
             self.db_channel = db_channel
