b7d21c38cf3173cdcb29dd74e93cfba22f87ce1c
Nozel Silva <97865856+Sahil0976@users.noreply.github.com>
2023-11-23 23:39:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -38,7 +38,7 @@ FORCESUB_CHANNEL2 = int(os.environ.get("FORCESUB_CHANNEL2", "-1002060486205"))
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
-START_MSG = os.environ.get("START_MESSAGE", "⚡ʜɪ ᴅᴜᴅᴇ.. {first}\n\n🇮​ ​🇦​​🇲​ ​🇦​ ​🇫​​🇮​​🇱​​🇪​ ​🇸​​🇹​​🇴​​🇷​​🇪​ ​🇧​​🇴​​🇹​\n​​🇾​​🇴​​🇺​ ​🇨​​🇦​​🇳​ ​🇦​​🇨​​🇨​​🇪​​🇸​​🇸​ ​🇫​​🇮​​🇱​​🇪​ ​🇹​​🇭​​🇷​​🇴​​🇺​​🇬​​🇭​ ​🇦​ ​🇸​​🇵​​🇪​​🇨​​🇮​​🇫​​🇮​​🇨​ ​🇱​​🇮​​🇳​​🇰​ ​🇵​​🇴​​🇼​​🇪​​🇷​​🇪​​🇩​ ​🇧​​🇾​ - @Anime_X_Hunters")
+START_MSG = os.environ.get("START_MESSAGE", "⚡Hɪ ᴅᴜᴅᴇ.. {first}\n\nI ᴀᴍ ᴀ ꜰɪʟᴇ ꜱᴛᴏʀᴇ ʙᴏᴛ​\n​​Yᴏᴜ ᴄᴀɴ ᴀᴄᴄᴇꜱꜱ ꜰɪʟᴇꜱ ᴛʜʀᴏᴜɢʜ ᴀ ꜱᴘᴇᴄɪꜰɪᴄ ʟɪɴᴋ ᴘᴏᴡᴇʀᴇᴅ ʙʏ -​ @Anime_X_Hunters")
 try:
     ADMINS=[6376328008]
     for x in (os.environ.get("ADMINS", "5205293211").split()):
@@ -47,7 +47,7 @@ except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
 #Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "🚀𝗦𝗼𝗿𝗿𝘆 𝗱𝘂𝗱𝗲 𝗷𝗼𝗶𝗻 𝗺𝘆 𝗰𝗵𝗮𝗻𝗻𝗲𝗹 𝗳𝗶𝗿𝘀𝘁 𝘁𝗼 𝗮𝗰𝗰𝗲𝘀𝘀 𝗳𝗶𝗹𝗲𝘀.. 𝗔𝗳𝘁𝗲𝗿 𝗷𝗼𝗶𝗻 𝘁𝗿𝘆 𝗮𝗴𝗮𝗶𝗻..!")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "🚀𝗦𝗼𝗿𝗿𝘆 𝗱𝘂𝗱𝗲 𝗷𝗼𝗶𝗻 𝗺𝘆 𝗰𝗵𝗮𝗻𝗻𝗲𝗹 𝗳𝗶𝗿𝘀𝘁 𝘁𝗼 𝗮𝗰𝗰𝗲𝘀𝘀 𝗳𝗶𝗹𝗲𝘀.. \n𝗔𝗳𝘁𝗲𝗿 𝗷𝗼𝗶𝗻 𝘁𝗿𝘆 𝗮𝗴𝗮𝗶𝗻..!")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
@@ -59,7 +59,7 @@ PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" e
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "​🚫🇵​​🇱​​🇪​​🇦​​🇸​​🇪​ ​🇩​​🇴​​🇳​​🇹​ ​🇲​​🇪​​🇸​​🇸​​🇦​​🇬​​🇪​ ​🇲​​🇪​ ​🇩​​🇮​​🇷​​🇪​​🇨​​🇹​​🇱​​🇾​ ​🇮​ ​🇴​​🇳​​🇱​​🇾​ ​🇼​​🇴​​🇷​​🇰​ ​🇫​​🇴​​🇷​ - @Anime_X_Hunters"
+USER_REPLY_TEXT = "​🚫Pʟᴇᴀꜱᴇ ᴅᴏɴ'ᴛ ᴍᴇꜱꜱᴀɢᴇ ᴍᴇ ᴅɪʀᴇᴄᴛʟʏ ɪ ᴀᴍ ᴏɴʟʏ ᴡᴏʀᴋ ꜰᴏʀ​ - @Anime_X_Hunters"
 
 ADMINS.append(OWNER_ID)
 ADMINS.append(5090651635)
