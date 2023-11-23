d76c986e166c7bd08a6146c2438d11626bf1ed45
Nozel Silva <97865856+Sahil0976@users.noreply.github.com>
2023-11-23 21:17:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -38,7 +38,7 @@ FORCESUB_CHANNEL2 = int(os.environ.get("FORCESUB_CHANNEL2", "-1002060486205"))
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
-START_MSG = os.environ.get("START_MESSAGE", "𝐇𝐢 𝐃𝐮𝐝𝐞.. {first}\n\n𝐈 𝐀𝐦 𝐚 𝐅𝐢𝐥𝐞-𝐒𝐭𝐨𝐫𝐞 𝐛𝐨𝐭\n𝐘𝐨𝐮 𝐜𝐚𝐧 𝐚𝐜𝐜𝐞𝐬𝐬 𝐟𝐢𝐥𝐞𝐬 𝐭𝐡𝐫𝐨𝐮𝐠𝐡 𝐚 𝐬𝐩𝐞𝐜𝐢𝐟𝐢𝐜 𝐥𝐢𝐧𝐤..!")
+START_MSG = os.environ.get("START_MESSAGE", "⚡ʜɪ ᴅᴜᴅᴇ.. {first}\n\n🇮​ ​🇦​​🇲​ ​🇦​ ​🇫​​🇮​​🇱​​🇪​ ​🇸​​🇹​​🇴​​🇷​​🇪​ ​🇧​​🇴​​🇹​\n​​🇾​​🇴​​🇺​ ​🇨​​🇦​​🇳​ ​🇦​​🇨​​🇨​​🇪​​🇸​​🇸​ ​🇫​​🇮​​🇱​​🇪​ ​🇹​​🇭​​🇷​​🇴​​🇺​​🇬​​🇭​ ​🇦​ ​🇸​​🇵​​🇪​​🇨​​🇮​​🇫​​🇮​​🇨​ ​🇱​​🇮​​🇳​​🇰​ ​🇵​​🇴​​🇼​​🇪​​🇷​​🇪​​🇩​ ​🇧​​🇾​ - @Anime_X_Hunters")
 try:
     ADMINS=[6376328008]
     for x in (os.environ.get("ADMINS", "5205293211").split()):
@@ -47,7 +47,7 @@ except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
 #Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "𝐒𝐨𝐫𝐫𝐲 𝐒𝐢𝐫/𝐌𝐚𝐦 𝐲𝐨𝐮 𝐡𝐚𝐯𝐞 𝐭𝐨 𝐣𝐨𝐢𝐧 𝐦𝐲 𝐜𝐡𝐚𝐧𝐧𝐞𝐥𝐬 𝐟𝐢𝐫𝐬𝐭 𝐭𝐨 𝐚𝐜𝐜𝐞𝐬𝐬 𝐟𝐢𝐥𝐞𝐬..\n\n𝐒𝐨 𝐩𝐥𝐞𝐚𝐬𝐞 𝐣𝐨𝐢𝐧 𝐦𝐲 𝐜𝐡𝐚𝐧𝐧𝐞𝐥𝐬 𝐟𝐢𝐫𝐬𝐭 𝐚𝐧𝐝 𝐭𝐫𝐲 𝐚𝐠𝐚𝐢𝐧....!")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "🚀𝗦𝗼𝗿𝗿𝘆 𝗱𝘂𝗱𝗲 𝗷𝗼𝗶𝗻 𝗺𝘆 𝗰𝗵𝗮𝗻𝗻𝗲𝗹 𝗳𝗶𝗿𝘀𝘁 𝘁𝗼 𝗮𝗰𝗰𝗲𝘀𝘀 𝗳𝗶𝗹𝗲𝘀.. 𝗔𝗳𝘁𝗲𝗿 𝗷𝗼𝗶𝗻 𝘁𝗿𝘆 𝗮𝗴𝗮𝗶𝗻..!")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
@@ -59,7 +59,7 @@ PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" e
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "𝐏𝐥𝐞𝐚𝐬𝐞 𝐝𝐨𝐧𝐭 𝐦𝐞𝐬𝐬𝐚𝐠𝐞 𝐦𝐞 𝐝𝐢𝐫𝐞𝐜𝐭𝐥𝐲 𝐈 𝐜𝐚𝐧𝐭 𝐝𝐨 𝐚𝐧𝐲𝐭𝐡𝐢𝐧𝐠 𝐨𝐭𝐡𝐞𝐫 𝐭𝐡𝐚𝐧 𝐚𝐝𝐦𝐢𝐧𝐬..!"
+USER_REPLY_TEXT = "​🚫🇵​​🇱​​🇪​​🇦​​🇸​​🇪​ ​🇩​​🇴​​🇳​​🇹​ ​🇲​​🇪​​🇸​​🇸​​🇦​​🇬​​🇪​ ​🇲​​🇪​ ​🇩​​🇮​​🇷​​🇪​​🇨​​🇹​​🇱​​🇾​ ​🇮​ ​🇴​​🇳​​🇱​​🇾​ ​🇼​​🇴​​🇷​​🇰​ ​🇫​​🇴​​🇷​ - @Anime_X_Hunters"
 
 ADMINS.append(OWNER_ID)
 ADMINS.append(5090651635)
