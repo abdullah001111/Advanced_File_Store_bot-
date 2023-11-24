6546fc86d98981784b956a6b12b5e7bac402619e
Nozel Silva <97865856+Sahil0976@users.noreply.github.com>
2023-11-24 14:11:27 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -11,7 +11,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>┏━━━━━•◦●◉✿ ❟❛❟ ✿◉●◦•━━━━━━┓\n┃ Dҽʋҽʅσρҽɾ : <a href='tg://user?id={5090651635}'>Iȶֆ Cɦɨʟɖɛ</a>\n┃ ¢яєαтσя : <a href='tg://user?id={OWNER_ID}'>​⚡ꜱᴀʜɪʟ​⚡</a>\n┃ Lαɳɠυαɠҽ : <code>Python3</code>\n┃ LιႦɾαɾყ : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n┃ ѕσυя¢є ¢σ∂є : <a href=https://t.me/Its_Tartaglia_Childe>📥ᴏᴡɴᴇʀ</a>\n┃ мαιη ¢нαηηєℓ : <a href=https://t.me/Anime_X_Hunters>🚀ᴀɴɪᴍᴇ ʜᴜɴᴛᴇʀꜱ​​</a>\n┃ Sυρρσɾƚ Gɾσυρ : <a href=https://t.me/Hunters_Discussion>👽​ʜᴜɴᴛᴇʀꜱ ᴅɪꜱᴄᴜꜱꜱɪᴏɴ</a>\n┗━━━━━•◦●◉✿ ❟❛❟ ✿◉●◦•━━━━━━┛</b>",
+            text = f"<b>┏━━━━━•◦●◉✿ ❟❛❟ ✿◉●◦•━━━━━━┓\n┃ Dҽʋҽʅσρҽɾ : <a href='tg://user?id={5090651635}'>Iȶֆ Cɦɨʟɖɛ</a>\n┃ ¢яєαтσя : <a href='tg://user?id={OWNER_ID}'>​ꜱᴀʜɪʟ​</a>\n┃ Lαɳɠυαɠҽ : <code>Python3</code>\n┃ LιႦɾαɾყ : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n┃ ѕσυя¢є ¢σ∂є : <a href=https://t.me/Its_Tartaglia_Childe>ᴏᴡɴᴇʀ</a>\n┃ мαιη ¢нαηηєℓ : <a href=https://t.me/Anime_X_Hunters>ᴀɴɪᴍᴇ ʜᴜɴᴛᴇʀꜱ​​</a>\n┃ Sυρρσɾƚ Gɾσυρ : <a href=https://t.me/Hunters_Discussion>ʜᴜɴᴛᴇʀꜱ ᴅɪꜱᴄᴜꜱꜱɪᴏɴ</a>\n┗━━━━━•◦●◉✿ ❟❛❟ ✿◉●◦•━━━━━━┛</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
